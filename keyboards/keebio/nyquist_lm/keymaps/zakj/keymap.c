a8cb0690dbab92f96dc4fb3155495349f04ce8df
Zak Johnson <me@zakj.net>
2026-08-17 20:09:35 -0700

    Make left pinky shift on hold, grave on tap

--- keyboards/keebio/nyquist_lm/keymaps/zakj/keymap.c
+++ keyboards/keebio/nyquist_lm/keymaps/zakj/keymap.c
@@ -1,6 +1,7 @@
 #include QMK_KEYBOARD_H
 
 #define CTL_ESC LCTL_T(KC_ESC)
+#define SFT_GRV LSFT_T(KC_GRV)
 #define CMD_LCB G(KC_LCBR)
 #define CMD_RCB G(KC_RCBR)
 #define CMD_LBR G(KC_LBRC)
@@ -10,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_4x12(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,  KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
         CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,          KC_H,  KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,  KC_M,   KC_COMM, KC_DOT,  KC_SLSH, SC_SENT,
+        SFT_GRV, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,  KC_M,   KC_COMM, KC_DOT,  KC_SLSH, SC_SENT,
         MO(2),   KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, MO(1),         MO(1), KC_SPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
     [1] = LAYOUT_ortho_4x12(
