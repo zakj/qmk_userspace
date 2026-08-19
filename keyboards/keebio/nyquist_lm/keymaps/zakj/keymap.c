3d3a3428923e051cbdb12cb5438d4197626caeec
Zak Johnson <me@zakj.net>
2026-08-18 18:40:54 -0700

    Add bootloader key on Fn+B

--- keyboards/keebio/nyquist_lm/keymaps/zakj/keymap.c
+++ keyboards/keebio/nyquist_lm/keymaps/zakj/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [2] = LAYOUT_ortho_4x12(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
         KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, CMD_LCB, CMD_RCB, XXXXXXX, XXXXXXX, XXXXXXX,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, CMD_LBR, CMD_RBR, XXXXXXX, XXXXXXX, _______,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,         XXXXXXX, CMD_LBR, CMD_RBR, XXXXXXX, XXXXXXX, _______,
         _______, _______, _______, _______, KC_SPC,  _______,         _______, _______, _______, _______, _______, _______
     )
 };
